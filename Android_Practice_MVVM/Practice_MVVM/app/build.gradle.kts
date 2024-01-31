95ea9704250b29f5c48e1398fb1d869abdd84d3a
Kim <kimgyudo93@gmail.com>
2024-01-31 14:11:32 +0900

    Android Practice ex code

--- Android_Practice_MVVM/Practice_MVVM/app/build.gradle.kts
+++ Android_Practice_MVVM/Practice_MVVM/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
 }
 
 android {
@@ -33,9 +34,13 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    dataBinding {
+        enable = true
+    }
 }
 
 dependencies {
+
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
@@ -44,4 +49,10 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // viewModel & liveData
+    val lifecycle_version = "2.7.0"
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
+
 }
--- Android_Practice_MVVM/Practice_MVVM/app/src/main/java/com/twproject/practice_mvvm/MainActivity.kt
+++ Android_Practice_MVVM/Practice_MVVM/app/src/main/java/com/twproject/practice_mvvm/MainActivity.kt
@@ -2,10 +2,22 @@ package com.twproject.practice_mvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.twproject.practice_mvvm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var mViewModel : MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        mViewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
+        binding.lifecycleOwner = this
+        binding.viewModel = mViewModel
     }
 }
